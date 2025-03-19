cab66633e7902b8ae8b6bbd70631a2d7013f6519
alex6036 <145665271+alex6036@users.noreply.github.com>
2025-03-19 17:49:12 +0000

    funcional

--- lanzador.py
+++ lanzador.py
@@ -3,26 +3,16 @@ from subclases.bicicleta import Bicicleta
 from subsubclases.camioneta import Camioneta
 from subsubclases.motocicleta import Motocicleta
 
-
 def lanzador_main():
+    print("\n=== Demostración de vehículos predefinidos ===")
     vehiculos = []
-
     vehiculos.append(Coche("rojo", 4, 150, 1200))
     vehiculos.append(Coche("azul", 4, 200, 2000))
     vehiculos.append(Bicicleta("verde", 2, "bh", "bmx", "urbana"))
     vehiculos.append(Bicicleta("blanca", 2, "orbea", "r2", "deportiva"))
     vehiculos.append(Camioneta("negro", 4, 100, 2200, "ford", "ranger", 1000))
-    vehiculos.append(Motocicleta("blanca",2,"Suzuki","GSXR", "Deportiva", 300, 999))
-    
-    
+    vehiculos.append(Motocicleta("blanca", 2, "Suzuki", "GSXR", "Deportiva", 300, 999))
     catalogar(vehiculos)
-    
-def catalogar(vehiculos):
-    for vehiculo in vehiculos:
-        print(f"Clase: {vehiculo.__class__.__name__}")
-        for atributo, valor in vehiculo.__dict__.items():
-            print(f"{atributo}: {valor}")
-        print()
 
 def catalogar(vehiculos, ruedas=None):
     if ruedas is not None:
